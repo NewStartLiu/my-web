dd936676d878142837bc182da6394312cbb62282
Liu Yi <liuyi@cmbi.com.hk>
2020-10-28 09:40:30 +0800

    去掉nginx配置，修改bgm地址

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
             controls="controls"
             loop="loop"
             ref="bgm"
-            src="/%E5%B2%91%E5%AE%81%E5%84%BF-%E8%BF%BD%E5%85%89%E8%80%85%EF%BC%88%E4%BC%B4%E5%A5%8F%EF%BC%89.mp3"
+            src="https://www.yohoo.xyz/music/%E5%B2%91%E5%AE%81%E5%84%BF-%E8%BF%BD%E5%85%89%E8%80%85%EF%BC%88%E4%BC%B4%E5%A5%8F%EF%BC%89.mp3"
         >
             你的浏览器不支持audio标签
         </audio>
